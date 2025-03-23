949f1265c34795491697fbdc06a5289ff74cdda9
Davi Gomes da Silva <104706788+DgDavi@users.noreply.github.com>
2025-03-23 01:37:23 -0300

    Update Ex09.java

--- Atividade01/src/Ex09.java
+++ Atividade01/src/Ex09.java
@@ -3,22 +3,36 @@ import java.util.Scanner;
 public class Ex09 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.print("Digite o primeiro número: ");
         int num1 = scanner.nextInt();
-
+        
         System.out.print("Digite o segundo número: ");
         int num2 = scanner.nextInt();
-
+        
         System.out.print("Digite o terceiro número: ");
         int num3 = scanner.nextInt();
-
-        int maior = Math.max(num1, Math.max(num2, num3));
-        int menor = Math.min(num1, Math.min(num2, num3));
-
+        
+        int maior = num1;
+        int menor = num1;
+        
+        if (num2 > maior) {
+            maior = num2;
+        }
+        if (num3 > maior) {
+            maior = num3;
+        }
+        
+        if (num2 < menor) {
+            menor = num2;
+        }
+        if (num3 < menor) {
+            menor = num3;
+        }
+        
         System.out.println("O maior número é: " + maior);
         System.out.println("O menor número é: " + menor);
-
+        
         scanner.close();
     }
 }
